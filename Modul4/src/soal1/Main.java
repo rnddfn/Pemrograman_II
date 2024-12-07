32b544d541cbd2289c38ca8929f3fdc56d60b41d
rnddfn <130640124+rnddfn@users.noreply.github.com>
2024-12-07 20:36:45 +0800

    Add files via upload

--- Modul4/src/soal1/Main.java
+++ Modul4/src/soal1/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Nama Hewan Peliharaan: ");
+		System.out.print("Nama Hewan Peliharaan: ");
 		String nama = input.nextLine();
-		System.out.println("ras: ");
+		System.out.print("ras: ");
 		String ras = input.nextLine();
 		HewanPeliharaan kucing = new HewanPeliharaan(nama, ras);
 		kucing.Display();
--- Modul4/src/soal2/Main.java
+++ Modul4/src/soal2/Main.java
@@ -9,23 +9,25 @@ public class Main {
 		System.out.println("Pilih jenis hewan yang ingin diinputkan: ");
 		System.out.println("1 = Kucing");
 		System.out.println("2 = Anjing");
-		System.out.println("Masukkan pilihan: ");
+		System.out.print("Masukkan pilihan: ");
 		int pilihan = input.nextInt();
 		input.nextLine();
-		System.out.println("Nama Hewan Peliharaan: " );
+		System.out.print("Nama Hewan Peliharaan: ");
 		String nama = input.nextLine();
-		System.out.println("ras: ");
+		System.out.print("ras: ");
 		String ras = input.nextLine();
 		if(pilihan == 1) {
-			System.out.println("Warna Bulu: ");
+			System.out.print("Warna Bulu: ");
 			String warnaBulu = input.nextLine();
+			System.out.println();
 			Kucing kucing = new Kucing(nama, ras, warnaBulu);
 			kucing.displayDetailKucing();
 		} else if(pilihan == 2) {
-			System.out.println("Warna Bulu: ");
+			System.out.print("Warna Bulu: ");
 			String warnaBulu = input.nextLine();
-			System.out.println("kemampuan: ");
+			System.out.print("kemampuan: ");
 			String[] kemampuan = new String[] {input.nextLine()};
+			System.out.println();
 			Anjing anjing = new Anjing(nama, ras, warnaBulu, kemampuan);
 			anjing.displayDetailAnjing();
 		}
